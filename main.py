108869a775fa7f8086e805aadde17912eecbb5cb
Mohsan4107 <mohsanmohammad46@gmail.com>
2024-06-08 12:24:23 +0100

    code for user guess and tests passed.

--- main.py
+++ main.py
@@ -10,4 +10,24 @@ def guess(x):
         elif guess > random_num:
             print('Sorry, number too high. Guess again')
     
-    print(f'Congratulations ! You have guess the correct number: {random_num}')
+    print(f'Congratulations ! You have guessed the correct number: {random_num}')
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    
+    print(f'The computer guessed the number: {guess} correctly!')
+
+guess(8)
+computer_guess(22)   
